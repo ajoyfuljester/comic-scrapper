8378461df3ebb70f3873756ddb8ac488df9d368e
ajoyfuljester <ajoyfuljester@protonmail.com>
2024-07-03 21:52:21 +0200

    added settings.json, SettingsWidget works

--- MainWidget.py
+++ MainWidget.py
@@ -1,14 +1,17 @@
 from PySide6 import QtWidgets
 from BrowserWidget import BrowserWidget
+from SettingsWidget import SettingsWidget
 
 class MainWidget(QtWidgets.QTabWidget):
     def __init__(self):
         super().__init__()
 
         self.browserWidget = BrowserWidget()
-
-
         self.addTab(self.browserWidget, 'Browser')
         self.browserWidget.search.setFocus()
+
+
+        self.settingsWidget = SettingsWidget()
+        self.addTab(self.settingsWidget, 'Settings')
 
 
--- SettingsWidget.py
+++ SettingsWidget.py
@@ -1,9 +1,20 @@
 from PySide6 import QtWidgets
+import json
 
 
 class SettingsWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout = QtWidgets.QFormLayout(self)
 
+        
+        with open('settings.json') as file:
+            config = json.loads(file.read())
+
+        
+        for k, v in config.items():
+            print(k, v)
+            self.formLayout.addRow(k, QtWidgets.QLabel(v))
+
+        print(self.formLayout.rowCount())
