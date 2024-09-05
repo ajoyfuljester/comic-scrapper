7d8feab87a3fda410b802bdf4119d31f50e2c28f
ajoyfuljester <ajoyfuljester@protonmail.com>
2024-09-05 20:34:44 +0200

    add getIssues in LocalUtils slowly but reliably i should finish this somewhen

--- Stuff/LocalUtils.py
+++ Stuff/LocalUtils.py
@@ -6,3 +6,9 @@ def getBooks():
     return next(os.walk(settings['PATH_TO_LOCAL']), [None, []])[1]
 
 
+def getIssues(name):
+    settings = SettingsUtils.loadSettings()
+
+    path = os.path.join(settings['PATH_TO_LOCAL'], name)
+
+    return next(os.walk(path), [None, []])[1]
