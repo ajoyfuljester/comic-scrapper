137269346c1a425cec73b28f5b3ca5fd843fb8d9
ajoyfuljester <ajoyfuljester@protonmail.com>
2024-09-10 21:12:59 +0200

    add deleteBook to LocalUtils

--- Stuff/LocalUtils.py
+++ Stuff/LocalUtils.py
@@ -2,6 +2,7 @@ from shutil import Error
 from . import SettingsUtils
 import os
 import json
+import shutil
 
 def getBooks():
     settings = SettingsUtils.loadSettings()
@@ -25,3 +26,8 @@ def _getBookInfo(bookName):
     info = {}
     raise Error('DO THIS LATER (AFTER I DO OTHER FUNCTIONS) (REMEMBER TO DELETE UNDERSCORE FROM THE NAME)')
     
+
+def deleteBook(bookName):
+    settings = SettingsUtils.loadSettings()
+    path = os.path.join(settings['PATH_TO_LIBRARY'], bookName)
+    shutil.rmtree(path)
